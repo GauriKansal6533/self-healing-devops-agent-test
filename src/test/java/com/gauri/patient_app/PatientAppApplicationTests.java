12d6eeedf92f10b29d634e136d3f6a3fdc711e4c
Gauri Kansal <gaurikansal6533@gmail.com>
2026-08-12 21:10:37 +0530

    Initial patient project

---
+++ src/test/java/com/gauri/patient_app/PatientAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.gauri.patient_app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PatientAppApplicationTests {
+
+	@Test
+	void additionShouldWork() {
+		int result = 2 + 2;
+
+		assertEquals(5, result);
+	}
+}
