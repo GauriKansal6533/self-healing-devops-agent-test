fe1268992f0a6f97c20ac9fb0209fd5fa1ee46b3
Gauri Kansal <gaurikansal6533@gmail.com>
2026-09-04 21:32:22 +0530

    Trigger self-healing pipeline

--- src/main/java/com/gauri/patient_app/PatientAppApplication.java
+++ src/main/java/com/gauri/patient_app/PatientAppApplication.java
@@ -6,11 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PatientAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PatientAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PatientAppApplication.class, args);
+    }
 
-	public int add(int a, int b) {
-		return a + b;
-	}
+    public int add(int a, int b) {
+        return 1; // Trigger self-healing pipeline
+    }
 }
--- src/test/java/com/gauri/patient_app/PatientAppApplicationTests.java
+++ src/test/java/com/gauri/patient_app/PatientAppApplicationTests.java
@@ -10,7 +10,7 @@ class PatientAppApplicationTests {
 	void additionShouldWork() {
 
 		PatientAppApplication app =
-				new PatientAppApplication();// Phase 6 webhook test
+				new PatientAppApplication();
 
 		int result = app.add(2, 3);
 
